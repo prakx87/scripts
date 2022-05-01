3dfd6306a95582d8b072e74bbe37bc1c186c5330
Prakyath Raj <prakx1987@gmail.com>
2022-05-01 22:41:13 +0530

    created backup v2 script using go

---
+++ backupv2/go.mod
@@ -0,0 +1,8 @@
+module doramax264.com/backupv2
+
+go 1.18
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jamf/go-mysqldump v0.7.1
+)
---
+++ backupv2/main.go
@@ -0,0 +1,65 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"os"
+	"database/sql"
+
+	"github.com/jamf/go-mysqldump"
+	"github.com/go-sql-driver/mysql"
+)
+
+var (
+	WarningLogger *log.Logger
+	InfoLogger *log.Logger
+	ErrorLogger *log.Logger
+)
+
+
+func init() {
+	// If file does not exist, then create it or append to file
+	file, err := os.OpenFile("/var/log/dxbackup.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+func main() {
+	// setup mysql connection object
+	fmt.Println("Create MySQL connection")
+	config := mysql.NewConfig()
+	config.User = "your-user"
+	config.Passwd = "pass"
+	config.DBName = "dxpass"
+	config.Net = "tcp"
+	config.Addr = "127.0.0.1"
+
+	dumpDir := "/root/backups"
+	dumpFilenameFormat := fmt.Sprintf("%s-20060102T150405", config.DBName)
+
+	db, err := sql.Open("mysql", config.FormatDSN())
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
+
+	fmt.Println("Successfully connected to MySQL database")
+
+	// use mysql object to take dump
+	dumper, err := mysqldump.Register(db, dumpDir, dumpFilenameFormat)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("DB taken successfully and saved at %s", dumpFilenameFormat)
+	dumper.Close()
+	
+	// upload dump to google and yandex drive
+	// cleanup older dumps
+}
